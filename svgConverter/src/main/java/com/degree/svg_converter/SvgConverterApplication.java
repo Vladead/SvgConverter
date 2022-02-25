facb83d6ca039b27e36b56a479b59de8b83cc9ec
Vladead <vladead8@yandex.ru>
2022-02-25 23:55:02 +0300

    Add svg converter module

---
+++ svgConverter/src/main/java/com/degree/svg_converter/SvgConverterApplication.java
@@ -0,0 +1,13 @@
+package com.degree.svg_converter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SvgConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SvgConverterApplication.class, args);
+	}
+
+}
