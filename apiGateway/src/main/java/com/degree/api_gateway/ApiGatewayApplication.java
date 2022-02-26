274e0dbb6a1874abfdb5579b439cdfc56fab5a3e
Vladead <vladead8@yandex.ru>
2022-02-26 11:52:22 +0300

    API Gateway settings

--- apiGateway/src/main/java/com/degree/api_gateway/ApiGatewayApplication.java
+++ apiGateway/src/main/java/com/degree/api_gateway/ApiGatewayApplication.java
@@ -2,8 +2,10 @@ package com.degree.api_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
