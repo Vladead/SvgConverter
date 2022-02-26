d4ff46ed70af14e71134afd8fd36cf7488ff0334
Vladead <vladead8@yandex.ru>
2022-02-26 11:50:03 +0300

    Specify settings for svg converter

--- svgConverter/src/main/java/com/degree/svg_converter/SvgConverterApplication.java
+++ svgConverter/src/main/java/com/degree/svg_converter/SvgConverterApplication.java
@@ -2,8 +2,10 @@ package com.degree.svg_converter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SvgConverterApplication {
 
 	public static void main(String[] args) {
