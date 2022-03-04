21b462f057debe333f2244158e8eef4fbaf505e8
Vladead <vladead8@yandex.ru>
2022-03-04 15:16:12 +0300

    Configure api gateway routes. Replacing deprecated zuul with spring gateway

--- svgConverter/src/main/java/com/degree/svg_converter/SvgConverterController.java
+++ svgConverter/src/main/java/com/degree/svg_converter/SvgConverterController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api")
+@RequestMapping("svg-converter")
 public class SvgConverterController {
     @GetMapping("test/hello")
     public String getHello() {
