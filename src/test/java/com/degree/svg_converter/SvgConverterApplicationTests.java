39be8b28c013813e4a0698ca39cfb4de3a984130
Vladead <vladead8@yandex.ru>
2022-03-12 23:25:15 +0300

    Basic spring boot service

---
+++ src/test/java/com/degree/svg_converter/SvgConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.degree.svg_converter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SvgConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
